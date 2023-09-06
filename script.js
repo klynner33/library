889029ffdd8b9fefe109cdde7679fde48979d5c6
klynner33 <113389068+klynner33@users.noreply.github.com>
2023-09-06 14:12:46 -0500

    Got 'add new book' button working so it will add input when clicked

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+document.querySelector('#add-book-button').addEventListener('click', addBookToLibrary)
+
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
 
+    
+    myLibrary.push();
 };
 
 function addBookToLibrary() {
-
+    document.querySelector('#card-title').innerText = document.querySelector('#title').value 
+    document.querySelector('#card-author').innerText = document.querySelector('#author').value
+    document.querySelector('#card-pages').innerText = document.querySelector('#pages').value
+    document.querySelector('#card-read').innerText = document.querySelector('#read-book').checked
 };
+
+// const myBook1 = {
+//     title: 'book title',
+//     author: 'b',
+//     pages: 2
+// }
+
+// document.querySelector('#card-title').innerText = myBook1.title;
